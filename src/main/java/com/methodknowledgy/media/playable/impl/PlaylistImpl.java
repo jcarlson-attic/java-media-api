9689a6926d0c581e65322e795ec9a79f93f44919
Jarrod Carlson <jarrod.carlson@gmail.com>
2008-11-18 05:41:32 +0000

    Identifying the methods from Vector that need to be overriden to manage the index

--- src/main/java/com/methodknowledgy/media/playable/impl/PlaylistImpl.java
+++ src/main/java/com/methodknowledgy/media/playable/impl/PlaylistImpl.java
@@ -1,5 +1,7 @@
 package com.methodknowledgy.media.playable.impl;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Vector;
 
 import com.methodknowledgy.media.playable.Playable;
@@ -65,7 +67,94 @@ public abstract class PlaylistImpl extends Vector<Playable> implements Playlist 
         }
         return null;
     }
+    
+    
+    
+    
+    
+
+    @Override
+    public synchronized boolean add(Playable o) {
+        // TODO Auto-generated method stub
+        return super.add(o);
+    }
+
+    @Override
+    public synchronized boolean addAll(Collection<? extends Playable> c) {
+        // TODO Auto-generated method stub
+        return super.addAll(c);
+    }
+
+    @Override
+    public synchronized boolean addAll(int index,
+            Collection<? extends Playable> c) {
+        // TODO Auto-generated method stub
+        return super.addAll(index, c);
+    }
+
+    @Override
+    public synchronized void addElement(Playable obj) {
+        // TODO Auto-generated method stub
+        super.addElement(obj);
+    }
+
+    @Override
+    public synchronized void insertElementAt(Playable obj, int index) {
+        // TODO Auto-generated method stub
+        super.insertElementAt(obj, index);
+    }
+
+    @Override
+    public synchronized Playable remove(int index) {
+        // TODO Auto-generated method stub
+        return super.remove(index);
+    }
+
+    @Override
+    public synchronized void removeAllElements() {
+        // TODO Auto-generated method stub
+        super.removeAllElements();
+    }
+
+    @Override
+    public synchronized void removeElementAt(int index) {
+        // TODO Auto-generated method stub
+        super.removeElementAt(index);
+    }
+
+    @Override
+    protected synchronized void removeRange(int fromIndex, int toIndex) {
+        // TODO Auto-generated method stub
+        super.removeRange(fromIndex, toIndex);
+    }
+
+    @Override
+    public synchronized Playable set(int index, Playable element) {
+        // TODO Auto-generated method stub
+        return super.set(index, element);
+    }
+
+    @Override
+    public synchronized void setElementAt(Playable obj, int index) {
+        // TODO Auto-generated method stub
+        super.setElementAt(obj, index);
+    }
+
+    @Override
+    public synchronized void setSize(int newSize) {
+        // TODO Auto-generated method stub
+        super.setSize(newSize);
+    }
+
+    @Override
+    public synchronized List<Playable> subList(int fromIndex, int toIndex) {
+        // TODO Auto-generated method stub
+        return super.subList(fromIndex, toIndex);
+    }
 
 
+    
+    
+    
 
 }
